4d766f8d96fad2ea92da9257c96e06e329e9c39b
Nathan Hoad <nathan@getoffmalawn.com>
2013-09-22 15:03:32 +1000

    Small codebase for parsing ICAP requests, 99% coverage.
    Starting fresh because this should all be simple, like it is now.

---
+++ icap.py
@@ -0,0 +1,370 @@
+import re
+
+from cStringIO import StringIO
+from collections import OrderedDict, namedtuple
+
+from werkzeug import cached_property
+from werkzeug.http import parse_dict_header
+
+# who could resist a class name like this?
+BodyPart = namedtuple('BodyPart', ['content', 'header'])
+RequestLine = namedtuple('RequestLine', 'method uri version')
+StatusLine = namedtuple('RequestLine', 'version code reason')
+
+
+class ParseState(object):
+    empty = 1
+    started = 2
+    body_started = 3
+    body_ended = 4
+
+
+class HeadersDict(OrderedDict):
+    def __init__(self, items=()):
+        OrderedDict.__init__(self)
+        for key, value in items:
+            self[key] = value
+
+    def __setitem__(self, key, value):
+        lkey = key.lower()
+
+        if lkey not in self:
+            OrderedDict.__setitem__(self, lkey, [(key, value)])
+        else:
+            OrderedDict.__getitem__(self, lkey).append((key, value))
+
+    def __getitem__(self, key):
+        return OrderedDict.__getitem__(self, key.lower())[0][1]
+
+    def get(self, key, default=None):
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return default
+
+    def getlist(self, key, default=list):
+        try:
+            return [v for k, v in OrderedDict.__getitem__(self, key.lower())]
+        except KeyError:
+            return default()
+
+    def replace(self, key, value):
+        lkey = key.lower()
+        OrderedDict.__setitem__(self, lkey, [(key, value)])
+
+    def __eq__(self, other):
+        if self.keys() != other.keys():
+            return False
+
+        for key in self.keys():
+            value = OrderedDict.__getitem__(self, key)
+            ovalue = OrderedDict.__getitem__(other, key)
+
+            if value != ovalue:
+                return False
+
+        return True
+
+
+class ChunkedMessage(object):
+    def __init__(self):
+        self.status_line = ''
+        self.headers = HeadersDict()
+        self.state = ParseState.empty
+
+    def started(self, set=False):
+        if set:
+            self.state = ParseState.started
+        return self.state != ParseState.empty
+
+    def headers_complete(self, set=False):
+        if set:
+            self.state = ParseState.body_started
+        return self.state > ParseState.started
+
+    def complete(self, set=False):
+        if set:
+            self.state = ParseState.body_ended
+        return self.state == ParseState.body_ended
+
+    def __iter__(self):
+        store_chunks = self.store_chunks
+        chunks = self._chunks
+
+        if self.complete():
+            for chunk in chunks:
+                yield chunk
+            return
+
+        while not self.complete():
+            line = self.stream.readline().strip()
+            try:
+                size, header = line.split(';', 1)
+            except ValueError:
+                size = line
+                header = ''
+
+            # needs support for trailers
+            size = int(size, 16)
+            if size:
+                # FIXME: non-crlf-endings
+                data = self.stream.read(size+2)  # +2 for CRLF
+                # FIXME: non-crlf-endings
+                chunk = BodyPart(data[:-2], header)
+                if store_chunks:
+                    chunks.append(chunk)
+                yield chunk
+            else:
+                self.complete(True)
+                # end of stream, get rid of trailing newline
+                self.stream.readline()
+
+    @classmethod
+    def from_kwargs(cls, store_chunks=False):
+        message = cls()
+        message.store_chunks = store_chunks
+        message._chunks = []
+        return message
+
+    @classmethod
+    def from_stream(cls, stream, **kwargs):
+        message = cls.from_kwargs(**kwargs)
+
+        complete = message.headers_complete
+        while not complete():
+            line = stream.readline()
+            message._feed_line(line)
+
+        assert message.headers_complete()
+
+        message.stream = stream
+        return message
+
+    @classmethod
+    def from_bytes(cls, bytes, **kwargs):
+        return cls.from_stream(StringIO(bytes), **kwargs)
+
+    def _feed_line(self, line):
+        if not self.started():
+            self.handle_status_line(line)
+        elif not self.headers_complete():
+            self.handle_header(line)
+
+    def handle_status_line(self, sline):
+        self.started(True)
+        self.sline = parse_start_line(sline.strip())
+
+    def handle_header(self, header):
+        # FIXME: non-clrf-endings
+        if not header.replace('\r\n', ''):
+            self.headers_complete(True)
+            return
+
+        # multiline headers
+        if header.startswith(('\t', ' ')):
+            k = self.headers.keys()[-1]
+            v = self.headers.pop(k)
+
+            # section 4.2 says that we MAY reduce whitespace down to a single
+            # character, so let's do it.
+            v = ' '.join((v, header.strip()))
+        else:
+            k, v = header.strip().split(':', 1)
+            k = k.rstrip()
+            v = v.lstrip()
+
+        self.headers[k] = v
+
+    @cached_property
+    def is_request(self):
+        return isinstance(self.sline, RequestLine)
+
+
+class ICAPRequest(ChunkedMessage):
+    @classmethod
+    def from_stream(cls, stream, **kwargs):
+        self = super(ICAPRequest, cls).from_stream(stream, **kwargs)
+
+        assert self.headers_complete()
+
+        parts = convert_offsets_to_sizes(self.encapsulated_header)
+
+        if self.is_respmod:
+            if 'req-hdr' in parts:
+                data = self.stream.read(parts['req-hdr'])
+                req = ChunkedMessage.from_bytes(data)
+                req_sline = req.sline
+                req_headers = req.headers
+            else:
+                req_sline = None
+                req_headers = HeadersDict()
+
+        missing_headers = ((self.is_reqmod and 'req-hdr' not in parts) or
+                           (self.is_respmod and 'res-hdr' not in parts))
+
+        if missing_headers:
+            m = ChunkedMessage.from_kwargs(store_chunks=not self.allow_204)
+            m.headers_complete(True)
+            m.stream = self.stream
+        else:
+            # NOTE: As it stands, we don't actually use req-hdr or res-hdr for
+            # reading the correct amount for headers here, but rely on the
+            # ChunkedMessage parsing.
+            m = ChunkedMessage.from_stream(self.stream)
+
+        self.encapsulated_message = m
+
+        if self.is_respmod:
+            m.request_sline = req_sline
+            m.request_headers = req_headers
+
+        if 'null-body' in parts:
+            m.complete(True)
+
+        return self
+
+    def __iter__(self):
+        for chunk in self.encapsulated_message:
+            yield chunk
+
+    @classmethod
+    def from_kwargs(cls, **kwargs):
+        return cls()
+
+    @cached_property
+    def encapsulated_header(self):
+        # this MUST throw a key error. It's a required header.
+        e = self.headers['encapsulated']
+        parsed = parse_encapsulated_field(e)
+        return parsed
+
+    @cached_property
+    def allow_204(self):
+        return '204' in self.headers.get('allow', '')
+
+    @cached_property
+    def is_reqmod(self):
+        return self.is_request and self.sline.method == 'REQMOD'
+
+    @cached_property
+    def is_respmod(self):
+        return self.is_request and not self.is_reqmod
+
+
+def convert_offsets_to_sizes(fields):
+    """Convert the results from func:`parse_encapsulated_field` to sizes
+    instead of offsets.
+
+    The Encapsulated header describes an encapsulated's section start point
+    relative to the start of the message body. This is confusing, so this
+    method provides a cleaner way of reading that info.
+
+    See RFC3507 4.4.1 for a further explanation.
+
+    :param chunks: :class:`collections.OrderedDict` of encapsulated chunk
+                   offsets.
+    :return: :class:`collections.OrderedDict` of sizes.
+    """
+    encapsulated_by_sizes = OrderedDict()
+
+    previous_offset = 0
+    previous_name = None
+
+    for name, offset in fields.iteritems():
+        size = offset - previous_offset
+        if previous_name:
+            encapsulated_by_sizes[previous_name] = size
+        previous_name = name
+        previous_offset = offset
+
+    # this is ALWAYS going to be the last header, and thus the body.
+    # we don't have a size for the body, so we read it in.
+    # use a sentinel of -1 to indicate it's of unknown size.
+    if previous_name == 'null-body':
+        encapsulated_by_sizes[previous_name] = 0
+    else:
+        encapsulated_by_sizes[previous_name] = -1
+
+    return encapsulated_by_sizes
+
+
+b = lambda body: '(%s-body|null-body)' % body
+
+#valid Encapsulated keys, according to RFC3507 section 4.4.1.
+#REQMOD  request  encapsulated_list: [reqhdr] reqbody
+#REQMOD  response encapsulatedlist: {[reqhdr] reqbody} | {[reshdr] resbody}
+#RESPMOD request  encapsulated_list: [reqhdr] [reshdr] resbody
+#RESPMOD response encapsulated_list: [reshdr] resbody
+#OPTIONS response encapsulated_list: optbody
+
+encapsulated_input_orders = {
+    'REQMOD':  '^(req-hdr )?%s$' % b('req'),
+    'RESPMOD': '^(req-hdr )?(res-hdr )?%s$' % b('res'),
+}
+
+del b
+
+
+def compile_encapsulated(fields):
+    for key, value in fields.iteritems():
+        fields[key] = re.compile(value)
+    return fields
+
+
+encapsulated_input_orders = compile_encapsulated(encapsulated_input_orders)
+
+
+def parse_encapsulated_field(raw_field):
+    """Parse an Encapsulated header, `raw_field`, and return it as an instance
+    of `collections.OrderedDict`, according to RFC3507.
+
+    Will raise :exc:`InvalidEncapsulatedHeadersError` if `raw_field` is not a
+    valid ICAP Encapsulated request header, according to RFC3507 section 4.4.1.
+
+    >>> from icap import parse_encapsulated_field
+    >>> parse_encapsulated_field('req-hdr=0, req-body=749')
+    OrderedDict([('req-hdr', 0), ('req-body', 749)])
+
+    :return: `collections.OrderedDict` containing the parsed encapsulated
+             sections.
+    """
+
+    parsed = parse_dict_header(raw_field, cls=OrderedDict)
+
+    keys = ' '.join(parsed)
+
+    for regex in encapsulated_input_orders.values():
+        if regex.match(keys):
+            return OrderedDict((key, int(value)) for (key, value)
+                               in parsed.iteritems())
+    else:
+        raise InvalidEncapsulatedHeadersError(raw_field)
+
+
+def parse_start_line(sline):
+    method, uri, version = parts = sline.split(' ', 2)
+
+    if method.upper().startswith(('HTTP', 'ICAP')):
+        version, code, reason = parts
+        return StatusLine(version.upper(), int(code), reason)
+    else:
+        return RequestLine(method.upper(), uri, version.upper())
+
+
+class InvalidEncapsulatedHeadersError(Exception):
+    """Represents a bug in an ICAP client, or the icap package, sending through
+    a malformed Encapsulated header.
+
+    If this is raised when parsing a request from a client, notify the
+    maintainer of that client.
+
+    If this is raised when serializing a response to a client, notify the
+    maintainer of this package.
+    """
+
+    def __init__(self, raw_field):
+        self.raw_field = raw_field
+
+        message = ("Encapsulated field does not comply with RFC3507: %s"
+                   % raw_field)
+        super(InvalidEncapsulatedHeadersError, self).__init__(message)
---
+++ tests/test_icap.py
@@ -0,0 +1,194 @@
+import pytest
+
+from icap import ChunkedMessage, ICAPRequest, HeadersDict
+
+def data_string(req_line, path):
+    parts = req_line, open('data/' + path).read()
+    return '\r\n'.join(p for p in parts if p)
+
+
+def assert_stream_consumed(message):
+    assert message.stream.read() == ''
+
+
+def assert_bodies_match(
+        message, expected_bodies, headers=None, total_length=None):
+
+    chunks = list(message)
+    if isinstance(expected_bodies, basestring):
+        expected_bodies = [expected_bodies]
+
+    assert [b.content for b in chunks] == expected_bodies
+
+    if total_length:
+        assert total_length == len(''.join(b.content for b in chunks))
+
+    if headers:
+        assert len(chunks) == len(headers)
+        for chunk, header in zip(chunks, headers):
+            assert chunk.header == header
+
+
+@pytest.mark.parametrize(('input_bytes', 'expected_values'), [
+    ('GET / HTTP/1.1\r\n\r\n4\r\nWiki\r\n5\r\npedia\r\nE\r\n in\r\n\r\nchunks.\r\n0\r\n\r\n',
+        dict(values=['Wiki', 'pedia', ' in\r\n\r\nchunks.'])),
+    ('GET / HTTP/1.1\r\n\r\n4;bar\r\nWiki\r\n5;foo\r\npedia\r\nE;qwer\r\n in\r\n\r\nchunks.\r\n0\r\n\r\n',
+        dict(values=['Wiki', 'pedia', ' in\r\n\r\nchunks.'], headers=['bar', 'foo', 'qwer'])),
+    ('GET / HTTP/1.1\r\n\r\n4;bar\r\nWiki\r\n5;foo\r\npedia\r\nE;qwer\r\n in\r\n\r\nchunks.\r\n0\r\n\r\n',
+        dict(values=['Wiki', 'pedia', ' in\r\n\r\nchunks.'], headers=['bar', 'foo', 'qwer'])),
+])
+def test_chunked_messages(input_bytes, expected_values):
+    m = ChunkedMessage.from_bytes(input_bytes)
+
+    assert_bodies_match(
+        m, expected_values.get('values'),
+        headers=expected_values.get('headers'), total_length=23)
+    assert_stream_consumed(m)
+
+
+def test_multiline_headers():
+    s = (
+        'GET / HTTP/1.1\r\n'
+        'Great-header: foo\r\n'
+        '\t       bar\r\n'
+        '\r\n'
+    )
+    m = ChunkedMessage.from_bytes(s)
+    assert m.headers['great-header'] == 'foo bar'
+
+
+def test_icap_parsing_simple():
+    expected = data_string('', 'icap_response_with_payload.response')
+
+    m = ICAPRequest.from_bytes(expected)
+
+    assert isinstance(m, ICAPRequest)
+
+    print '-----'
+    print expected
+    print '-----'
+    print 'parent headers', m.headers
+    print 'child headers', m.encapsulated_message.headers
+    assert_bodies_match(m, 'This is data that was returned by an origin server, but with\r\nvalue added by an ICAP server.')
+    assert_stream_consumed(m)
+
+
+def test_icap_parsing_complex():
+    'Tests req-hdr, res-hdr, res-body'
+
+    expected = data_string('', 'icap_request_with_two_header_sets.request')
+    expected_headers = HeadersDict([
+        ('Host', 'icap.example.org'),
+        ('Encapsulated', 'req-hdr=0, res-hdr=137, res-body=296'),
+    ])
+
+    expected_child_request_headers = HeadersDict([
+        ('Host', 'www.origin-server.com'),
+        ('Accept', 'text/html, text/plain, image/gif'),
+        ('Accept-Encoding', 'gzip, compress'),
+    ])
+
+    expected_child_headers = HeadersDict([
+        ('Date', 'Mon, 10 Jan 2000 09:52:22 GMT'),
+        ('Server', 'Apache/1.3.6 (Unix)'),
+        ('ETag', '"63840-1ab7-378d415b"'),
+        ('Content-Type', 'text/html'),
+        ('Content-Length', '51'),
+    ])
+
+    m = ICAPRequest.from_bytes(expected)
+
+    assert isinstance(m, ICAPRequest)
+
+    child = m.encapsulated_message
+
+    assert ' '.join(m.sline) == 'RESPMOD icap://icap.example.org/satisf ICAP/1.0'
+    assert ' '.join(child.request_sline) == 'GET /origin-resource HTTP/1.1'
+    assert ' '.join(map(str, child.sline)) == 'HTTP/1.1 200 OK'
+
+    assert m.headers == expected_headers
+    assert child.request_headers == expected_child_request_headers
+    assert child.headers == expected_child_headers
+
+    assert_bodies_match(m, 'This is data that was returned by an origin server.')
+    assert_stream_consumed(m)
+
+
+@pytest.mark.parametrize(('test_file', 'expected_values'), [
+    ('icap_reqbody.response', dict(body='This is data that was returned by an origin server.')),
+    ('icap_reqhdr_resbody.response', dict(req_parts=True, body=['This is data that was returned by an origin server.'])),
+    ('icap_resbody.response', dict(body=['This is data that was returned by an origin server.'])),
+    ('icap_nullbody.response', dict()),
+])
+def test_icap_parsing_stupid(test_file, expected_values):
+    '''Test for the stupidest combinations of Encapsulated headers that I can come up with.'''
+    data = data_string('', test_file)
+
+    print test_file
+    print '----'
+    print data
+    print '----'
+
+    m = ICAPRequest.from_bytes(data)
+    child = m.encapsulated_message
+
+    has_headers = expected_values.get('headers', False)
+    has_req_parts = expected_values.get('req_parts', False)
+    body = expected_values.get('body')
+
+    if has_req_parts:
+        assert m.is_respmod
+        assert child.request_sline
+        assert child.request_headers
+
+    assert bool(child.headers) == has_headers
+
+    if body:
+        assert_bodies_match(m, body)
+    else:
+        assert_bodies_match(m, [])
+
+    assert_stream_consumed(m)
+
+
+def test_HeadersDict():
+    h = HeadersDict()
+    h['Foo'] = 'bar'
+    h['Foo'] = 'baz'
+
+    assert h['foo'] == 'bar'
+    assert h['FOO'] == 'bar'
+    assert h.get('foo') == 'bar'
+    assert h.get('bar') is None
+    assert h.get('baz', 'asdf') == 'asdf'
+    assert h.getlist('foo') == ['bar', 'baz']
+    assert h.getlist('bar') == []
+
+    h.replace('Foo', 'bar')
+    assert h.getlist('Foo') == ['bar']
+
+    b = HeadersDict()
+
+    for i in xrange(6):
+        b['Foo'] = 'bar'
+
+    a = HeadersDict([
+        ('Foo', 'bar'),
+        ('Foo', 'bar'),
+        ('Foo', 'bar'),
+        ('Foo', 'bar'),
+        ('Foo', 'bar'),
+        ('Foo', 'bar'),
+    ])
+
+    c = HeadersDict([
+        ('lamp', 'i love lamp'),
+    ])
+
+    d = HeadersDict([
+        ('lamp', 'i dont love lamp'),
+    ])
+
+    assert a == b
+    assert a != c
+    assert c != d
