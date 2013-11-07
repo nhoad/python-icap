301fb18653651fcc1583ae86b95141277de2530b
Nathan Hoad <nathan@getoffmalawn.com>
2013-11-07 20:27:06 +1100

    Also do a test to make sure we don't crash on modifying unsuitable requests.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,13 +1,15 @@
-import urllib2
-
 from examples import withoutmypants
 from icap.parsing import HTTPMessageParser
 
 
-def test_withoutmypants():
+def test_withoutmypants_modified():
     request = HTTPMessageParser.from_bytes("GET http://google.com/?q=foo HTTP/1.1\r\n\r\n")
     withoutmypants.reqmod(request)
-
     print str(request)
-
     assert "?q=foo+without+my+pants" in str(request)
+
+def test_withoutmypants_not_modified():
+    request = HTTPMessageParser.from_bytes("GET http://google.com/?qq=foo HTTP/1.1\r\n\r\n")
+    withoutmypants.reqmod(request)
+    print str(request)
+    assert "without+my+pants" not in str(request)
