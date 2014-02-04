9fe9a443657480eb719f01aab57898780c0f154c
Nathan Hoad <nathan@getoffmalawn.com>
2014-02-04 21:08:02 +1100

    Another benchmark, using a 200kb response.

--- tests/benchmarks.py
+++ tests/benchmarks.py
@@ -5,6 +5,8 @@ from icap import ICAPRequestParser, HTTPMessageParser
 
 def benchmark(count, maximum_time, *args, **kwargs):
     def inner(func):
+        request = kwargs.get('request')
+
         s = time.time()
         for _ in range(count):
             func(*args, **kwargs)
@@ -13,17 +15,22 @@ def benchmark(count, maximum_time, *args, **kwargs):
         total = e - s
         per_request = total / count
 
-        print('took %.5f seconds (%.10f per call for %d calls)' % (total, per_request, count))
+        print('took {:.5f} seconds to process {:,d} bytes ({:.10f} seconds per call for {} calls)'.format(total, len(request)*count, per_request, count))
         assert per_request < maximum_time
         return func
     return inner
 
 
-@benchmark(200, 0.15, request=open('data/request_with_http_response_and_payload.request', 'rb').read())
+@benchmark(2200, 0.0005, request=open('data/request_with_http_response_and_payload.request', 'rb').read())
 def benchmark_ICAP_parsing(request):
     ICAPRequestParser.from_bytes(request)
 
 
-@benchmark(1000, 0.20, request=open('data/http_request_with_payload.request', 'rb').read())
+@benchmark(6000, 0.0002, request=open('data/http_request_with_payload.request', 'rb').read())
+def benchmark_HTTP_parsing(request):
+    HTTPMessageParser.from_bytes(request)
+
+
+@benchmark(1800, 0.0006, request=open('data/ninemsn.com.au', 'rb').read())
 def benchmark_HTTP_parsing(request):
     HTTPMessageParser.from_bytes(request)
